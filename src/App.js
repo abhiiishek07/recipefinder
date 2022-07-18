491259a55553a59b54a824acc360cc84a3277bce
Abhishek <abhi885990@gmail.com>
2022-07-18 16:01:22 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <BrowserRouter basename="/recipefinder">
         <Search />
         <Category />
         <Pages />
